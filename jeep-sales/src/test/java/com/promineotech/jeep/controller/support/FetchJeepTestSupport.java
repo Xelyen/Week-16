48216a967ec9db0e742be93a9a0fff09b0d19bc5
unknown <khoang5901@gmail.com>
2023-01-21 14:07:13 -0600

    Code

---
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
@@ -0,0 +1,18 @@
+package com.promineotech.jeep.controller.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+public class FetchJeepTestSupport extends BaseTest {
+  
+}
